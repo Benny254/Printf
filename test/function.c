2e6ecdae69fbdd4616fa3b923dabf39595105937
Benny254 <asshimbenny01@gmail.com>
2023-08-20 08:12:07 -0700

    update

--- test/function.c
+++ test/function.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/********************* PRINT CHAR **********************/
+
 int print_char(va_list types, char buffer[],
     int flags, int width, int precision, int size) {
     char c = va_arg(types, int);
-    return (handle_write_char(c, buffer, flags, width, precision, size));
+
+    // Placeholder: Call handle_write_char or equivalent
+    // return handle_write_char(c, buffer, flags, width, precision, size);
 }
 
+/************************* PRINT A STRING *************************/
 int print_string(va_list types, char buffer[],
     int flags, int width, int precision, int size) {
     int length = 0, i;
@@ -13,25 +18,27 @@ int print_string(va_list types, char buffer[],
 
     // ... (remaining code of print_string)
 
-    return (write(1, str, length));
+    // Placeholder: Return the result of write() or equivalent
+    // return write(1, str, length);
 }
 
+/************************* PRINT PERCENT SIGN *************************/
 int print_percent(va_list types, char buffer[],
     int flags, int width, int precision, int size) {
-    return (write(1, "%%", 1));
+    // Placeholder: Return the result of write() or equivalent
+    // return write(1, "%%", 1);
 }
 
+/************************* PRINT INT *************************/
 int print_int(va_list types, char buffer[],
     int flags, int width, int precision, int size) {
-    // ... (remaining code of print_int)
-
-    return (write_number(is_negative, i, buffer, flags, width, precision, size));
+    // Placeholder: Call convert_size_number and write_number or equivalent
+    // return write_number(is_negative, i, buffer, flags, width, precision, size);
 }
 
+/************************* PRINT BINARY *************************/
 int print_binary(va_list types, char buffer[],
     int flags, int width, int precision, int size) {
-    // ... (remaining code of print_binary)
-
-    return (count);
+    // Placeholder: Perform binary conversion and output, return count
 }
 
