b378df4799c2989820bab8bf56028bd0b17cc0b1
Benny254 <asshimbenny01@gmail.com>
2023-08-22 14:42:39 -0700

    function.c

--- test/function.c
+++ test/function.c
@@ -1,44 +1,19 @@
 #include "main.h"
 
-/********************* PRINT CHAR **********************/
+/**
+ * print_integer - the function for %d or %i
+ *
+ * Description: the num and *count
+ *
+ * Return: the count
+ */
+int print_integer(int num, int *count)
+{
+	char buffer[BUFFERSIZE];
+	int len = snprintf(buffer, sizeof(buffer), "%d", num);
 
-int print_char(va_list types, char buffer[],
-    int flags, int width, int precision, int size) {
-    char c = va_arg(types, int);
-
-    // Placeholder: Call handle_write_char or equivalent
-    // return handle_write_char(c, buffer, flags, width, precision, size);
+	if (len < 0 || (size_t)len >= sizeof(buffer))
+		return (-1);
+	*count += write(1, buffer, len);
+	return (*count);
 }
-
-/************************* PRINT A STRING *************************/
-int print_string(va_list types, char buffer[],
-    int flags, int width, int precision, int size) {
-    int length = 0, i;
-    char *str = va_arg(types, char *);
-
-    // ... (remaining code of print_string)
-
-    // Placeholder: Return the result of write() or equivalent
-    // return write(1, str, length);
-}
-
-/************************* PRINT PERCENT SIGN *************************/
-int print_percent(va_list types, char buffer[],
-    int flags, int width, int precision, int size) {
-    // Placeholder: Return the result of write() or equivalent
-    // return write(1, "%%", 1);
-}
-
-/************************* PRINT INT *************************/
-int print_int(va_list types, char buffer[],
-    int flags, int width, int precision, int size) {
-    // Placeholder: Call convert_size_number and write_number or equivalent
-    // return write_number(is_negative, i, buffer, flags, width, precision, size);
-}
-
-/************************* PRINT BINARY *************************/
-int print_binary(va_list types, char buffer[],
-    int flags, int width, int precision, int size) {
-    // Placeholder: Perform binary conversion and output, return count
-}
-
